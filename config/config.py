52acbc0c61c13c2b599cc78271a2231b7bcc8b36
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-10-20 12:11:37 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -75,7 +75,7 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "1808943146").split())
+    map(int, getenv("OWNER_ID", "7154103754").split())
 )  # Input type must be interger
 
 
